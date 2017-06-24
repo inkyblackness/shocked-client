bf827ce55a425a5162354180812ff15c215998e2
Christian Haas <christian.haas@sevensuns.at>
2017-06-24 09:22:38 +0200

    changed height display formatting (user perceived height has focus)

--- editor/modes/LevelMapMode.go
+++ editor/modes/LevelMapMode.go
@@ -553,7 +553,7 @@ func (mode *LevelMapMode) heightUnitToString(value int64) string {
 	tileHeights := []float64{32.0, 16.0, 8.0, 4.0, 2.0, 1.0, 0.5, 0.25}
 	heightShift := mode.levelAdapter.HeightShift()
 
-	return fmt.Sprintf("%v = %.3f tile(s)", value, (float64(value)*tileHeights[heightShift])/32.0)
+	return fmt.Sprintf("%.3f tile(s)  - raw: %v", (float64(value)*tileHeights[heightShift])/32.0, value)
 }
 
 // SetActive implements the Mode interface.
